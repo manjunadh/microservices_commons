a9115279e0157187a0e265817fe9c17191540a86
Manjunadh Veeravalli <P7111009@NESSINBLR.COM>
2017-03-17 11:44:42 +0530

    util service

---
+++ src/main/java/com/fgl/microservices/commons/util/LambdaServletFilter.java
@@ -0,0 +1,12 @@
+package com.fgl.microservices.commons.util;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+
+
+public interface LambdaServletFilter extends Filter {
+
+    default public void init(FilterConfig filterConfig) throws ServletException {}
+    default public void destroy() {}
+}
